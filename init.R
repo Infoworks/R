4825ffcd60a0d2f4faba3104c9f2efed08eaeeeb
Abhilash L L <abhilash@infoworks.io>
2016-02-18 21:53:54 +0530

    updating from demo server code

--- init.R
+++ init.R
@@ -1,5 +1,5 @@
 InitMongo <- function(db="infoworks-new", host="localhost", port='27017', username='infoworks', password='IN11**rk')
-  {
+{
   # get the values from config file available on iw rest api
 
   # mongo initialize
@@ -11,7 +11,7 @@ InitMongo <- function(db="infoworks-new", host="localhost", port='27017', userna
 
   library(rmongodb)
   iwmongodb <<- mongo.create(
-    host=host, name="", username=username, password=password, db=db)
+  host=host, name="", username=username, password=password, db=db)
 
   # load source names (source tables will have to be added explicity later)
   sources = dbGetQueryForKeys(iwmongo, "sources", "", "{'name':1,'hdfs_path':1}", 0, 1000)
@@ -27,34 +27,51 @@ InitMongo <- function(db="infoworks-new", host="localhost", port='27017', userna
     varname <- paste("iw.sources", source_name, sep=".")
     assign(varname, list(name=source_name, id=source_id, hdfs_path=hdfs_path), envir = .GlobalEnv)
   }
+
+  # load source names (source tables will have to be added explicity later)
+  dms = dbGetQueryForKeys(iwmongo, "datamodels", "", "{'name':1,'hdfs_path':1}", 0, 1000)
+  # TO DEBUG for 1 source sources = dbGetQueryForKeys(iwmongo, "sources", '{"name":"sparktest1"}', "{'name':1,'hdfs_path':1, 'sourceType':1}", 0, 1000)
+  # assign("iw.sources", sources, envir = .GlobalEnv)
+  for (i in 1:length(dms[[1]])) {
+    dm_name = dms$name[[i]]
+    dm_id = dms$X_id[[i]]
+    hdfs_path = dms$hdfs_path[[i]]
+    #print("init hdfs_path")
+    #print(hdfs_path)
+    #print(source_name)
+    varname <- paste("iw.datamodels", dm_name, sep=".")
+    assign(varname, list(name=dm_name, id=dm_id, hdfs_path=hdfs_path), envir = .GlobalEnv)
+  }
 }
 
-InitSpark <- function(sparkMaster = 'local', hdfs_url = "hdfs://ip-172-30-0-110.ec2.internal:8020") {
+InitSpark <- function(sparkMaster = 'local', hdfs_url = "hdfs://ip-172-30-0-245.ec2.internal:8020") {
   #------ spark initialize ----
 
   # Set this to where Spark is installed
-  # SKH Commented Sys.setenv(SPARK_HOME="/home/rstudio/spark/spark-1.5.1-bin-hadoop2.6")
-  Sys.setenv(SPARK_HOME="/usr/hdp/current/spark-client")
+  Sys.setenv(SPARK_HOME="/home/rstudio/spark-1.4.0-bin-hadoop2.6")
+  #Sys.setenv(SPARK_HOME="/home/rstudio/sparkcode/spark-1.6.0")
+  #Sys.setenv(SPARK_HOME="/usr/hdp/current/spark-client")
 
   # This line loads SparkR from the installed directory
   .libPaths(c(file.path(Sys.getenv("SPARK_HOME"), "R", "lib"), .libPaths()))
   library(SparkR)
 
   Sys.setenv('SPARKR_SUBMIT_ARGS'='--packages com.databricks:spark-avro_2.10:2.0.1 sparkr-shell')
+  #Sys.setenv('SPARKR_SUBMIT_ARGS'='"--jars" " sparkr-shell')
 
   sc <<- sparkR.init(master=sparkMaster)
-  #sqlContext <<- sparkRSQL.init(sc)
+  sqlContext <<- sparkRSQL.init(sc)
   hivecontext <<- sparkRHive.init(sc) #[Spark SQL is not built with Hive support]
 
   # internal ip to be used in hadoop filepath
   hdfs_url <<- hdfs_url
+  print(hdfs_url)
 }
 
 Init <- function(db="infoworks-new", host="localhost", port='27017', username='infoworks', password='IN11**rk',
-                 sparkMaster = 'local', hdfs_url = "hdfs://ip-172-30-0-110.ec2.internal:8020") {
-
+sparkMaster = 'local', hdfs_url = "hdfs://ip-172-30-0-245.ec2.internal:8020") {
   InitMongo(db="infoworks-new", host="localhost", port='27017', username='infoworks', password='IN11**rk')
-  InitSpark(sparkMaster = 'local', hdfs_url = "hdfs://ip-172-30-0-110.ec2.internal:8020")
+  InitSpark(sparkMaster = 'local', hdfs_url = "hdfs://ip-172-30-0-245.ec2.internal:8020")
 }
 
 # ===============================
@@ -62,6 +79,3 @@ Init <- function(db="infoworks-new", host="localhost", port='27017', username='i
 # Use config file or get config parameters from iw web app api
 
 Init()
-
-
-
--- mongo_api.R
+++ mongo_api.R
@@ -4,35 +4,36 @@ LoadSources <- function(...) {
   args = as.list(match.call())
   args_sources = args[2:length(args)]
   args_sources = lapply(args_sources, deparse)
-  
+
   source_names = paste("'", paste(lapply(args_sources, LastDotValue), collapse="', '"), "'", sep="")
   print(source_names)
-  
-  sources = dbGetQueryForKeys(iwmongo, "sources", paste("{name: { $in: [", source_names, " ]}}", sep=""), "{'name':1, 'tables':1}", 0, 1000)
+
+  sources = dbGetQueryForKeys(iwmongo, "sources", paste("{name: { $in: [", source_names, " ]}}", sep=""), "{'name':1, 'tables':1, 'hdfs_path':1}", 0, 1000)
   assign("iw.sources", sources, envir = .GlobalEnv)
-  
+
   sources_loaded = c()
   for (i in 1:length(sources[[1]])) {
     source_name = sources$name[[i]]
     source_id = sources$X_id[[i]]
-    hdfs_path = sources$hdfs_path 
+    hdfs_path = sources$hdfs_path[[i]]
+    print("hdfs path ")
+    print( hdfs_path)
     tables = sources$tables[[i]]
-    
+
     # removed initial iw.sources from variable names
-    varname <- paste("", source_name, sep="")
-    assign(varname, list(name=source_name, id=source_id), envir = .GlobalEnv)
-    #varname <- paste(varname, "tables", sep=".")
-    
-    # get table details
-    tables = dbGetQueryForKeys(iwmongo, "tables", 
-                               paste("{'source': { '$oid': '", source_id, "' }}", sep=""), 
-                               "{'table':1, 'columns':1, 'fk':1, 'source':1}", 0, 10000)
-    
-    
+    #varname <- paste("", source_name, sep="")
+    varname <- source_name
+    assign(varname, list(name=source_name, id=source_id, hdfs_path=hdfs_path), envir = .GlobalEnv)
+
+    # get table detail
+    tables = dbGetQueryForKeys(iwmongo, "tables",
+    paste("{'source': { '$oid': '", source_id, "' }}", sep=""),
+    "{'table':1, 'columns':1, 'fk':1, 'source':1}", 0, 10000)
+
     sources_loaded = c(sources_loaded, paste(source_name, nrow(tables), sep=", tables: "))
     if (nrow(tables) == 0) next
     # for (j in 1:0) runs the loop twice, so the following for loop is still executed
-    
+
     for (j in 1:nrow(tables)) {
       table_name = tables$table[[j]]
       table_id = tables$X_id[[j]]
@@ -40,18 +41,18 @@ LoadSources <- function(...) {
       table_columns_json = fromJSON(table_columns)
       table_col_count = length(table_columns_json)
       fk = tryCatch(fromJSON(tables$fk[[j]]), error = function(e) {''})
-      
+
       varname1 <- paste(varname, table_name, sep=".")
-      assign(varname1, 
-             list(varname=varname1, name=table_name, id=table_id, 
-                  table_col_count=table_col_count, fk=fk, source=tables$source[[j]]),
-             envir = .GlobalEnv)
+      assign(varname1,
+      list(varname=varname1, name=table_name, id=table_id,
+      table_col_count=table_col_count, fk=fk, source=tables$source[[j]]),
+      envir = .GlobalEnv)
       #varname1 <- paste(varname1, "column", sep=".")
       #assign(varname1, table_columns_json)
-      
+
       if (length(table_columns_json) == 0) next
       for (k in 1:length(table_columns_json)) {
-        column_name = table_columns_json[[k]][[1]]
+        column_name = table_columns_json[[k]][["name"]]
         varname2 <- paste(varname1, column_name, sep=".")
         cols = table_columns_json[[k]]
         cols[["index"]] = k
@@ -61,33 +62,100 @@ LoadSources <- function(...) {
       }
     }
   }
-  
+
   print("Sources loaded:")
-  print(sources_loaded)    
+  print(sources_loaded)
 }
 
 UnloadSources <- function() {
-  
+
 }
 
 Columns <- function(table) {
-  
+
 }
 
 Tables <- function(source) {
-  
+
 }
 
 Sources <- function(domain) {
-  
+
 }
 
 TableDetails <- function(table) {
-  
+
 }
 
 SourceDetails <- function(source) {
-  
+
+}
+
+LoadDatamodels <- function(...) {
+  args = as.list(match.call())
+  args_datamodels = args[2:length(args)]
+  args_datamodels = lapply(args_datamodels, deparse)
+
+  datamodel_names = paste("'", paste(lapply(args_datamodels, LastDotValue), collapse="', '"), "'", sep="")
+  print(datamodel_names)
+
+  datamodels = dbGetQueryForKeys(iwmongo, "datamodels", paste("{name: { $in: [", datamodel_names, " ]}}", sep=""), "{'name':1, 'tables':1, 'hdfs_path':1}", 0, 1000)
+  assign("iw.datamodels", datamodels, envir = .GlobalEnv)
+
+  datamodels_loaded = c()
+  for (i in 1:length(datamodels[[1]])) {
+    datamodel_name = datamodels$name[[i]]
+    datamodel_id = datamodels$X_id[[i]]
+    hdfs_path = datamodels$hdfs_path[[i]]
+    print("hdfs path ")
+    print( hdfs_path)
+    tables = datamodels$tables[[i]]
+
+    # removed initial iw.datamodels from variable names
+    #varname <- paste("", datamodel_name, sep="")
+    varname <- datamodel_name
+    assign(varname, list(name=datamodel_name, id=datamodel_id, hdfs_path=hdfs_path), envir = .GlobalEnv)
+
+    # get table detail
+    tables = dbGetQueryForKeys(iwmongo, "tables",
+    paste("{'datamodel': { '$oid': '", datamodel_id, "' }}", sep=""),
+    "{'table':1, 'columns':1, 'fk':1, 'datamodel':1}", 0, 10000)
+
+    datamodels_loaded = c(datamodels_loaded, paste(datamodel_name, nrow(tables), sep=", tables: "))
+    if (nrow(tables) == 0) next
+    # for (j in 1:0) runs the loop twice, so the following for loop is still executed
+
+    for (j in 1:nrow(tables)) {
+      table_name = tables$table[[j]]
+      table_id = tables$X_id[[j]]
+      table_columns = tables$columns[[j]]
+      table_columns_json = fromJSON(table_columns)
+      table_col_count = length(table_columns_json)
+      fk = tryCatch(fromJSON(tables$fk[[j]]), error = function(e) {''})
+
+      varname1 <- paste(varname, table_name, sep=".")
+      assign(varname1,
+      list(varname=varname1, name=table_name, id=table_id,
+      table_col_count=table_col_count, fk=fk, datamodel=tables$datamodel[[j]]),
+      envir = .GlobalEnv)
+      #varname1 <- paste(varname1, "column", sep=".")
+      #assign(varname1, table_columns_json)
+
+      if (length(table_columns_json) == 0) next
+      for (k in 1:length(table_columns_json)) {
+        column_name = table_columns_json[[k]][["name"]]
+        varname2 <- paste(varname1, column_name, sep=".")
+        cols = table_columns_json[[k]]
+        cols[["index"]] = k
+        cols[["varname"]] = varname2
+        cols[["table_col_count"]] = table_col_count
+        assign(varname2, cols, envir = .GlobalEnv)
+      }
+    }
+  }
+
+  print("Datamodels loaded:")
+  print(datamodels_loaded)
 }
 
 #------- Helper Functions ----------
