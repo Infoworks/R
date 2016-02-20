9bbf7f192309fc8ff321d27dbf82d5bffcffa986
Abhilash L L <abhilash@infoworks.io>
2016-02-20 13:33:45 +0530

    cvs mo phase 1

--- cvs/cvs_mo_transformations.R
+++ cvs/cvs_mo_transformations.R
@@ -20,7 +20,7 @@ prepare_p1_ClaimBaseTableDF <- function(iwdfs) {
 
   #load pharmacy claim
   iwdfs.V_PHMCY_CLM <- SelectDF(POC_MBR_OPPTY.V_PHMCY_CLM)
-  iwdfs.V_PHMCY_CLM <- aliasDF(iwdfs.V_PHMCY_CLM, "PHMCY")
+  iwdfs.V_PHMCY_CLM <- aliasDF(iwdfs.V_PHMCY_CLM, "PHMCY", TRUE)
 
   #add filters
   #COB_CD = 'PRMRY'
@@ -30,13 +30,13 @@ prepare_p1_ClaimBaseTableDF <- function(iwdfs) {
 
   #load mbr acct
   iwdfs.V_MBR_ACCT_HIST <- SelectDF(POC_MBR_OPPTY.V_MBR_ACCT_HIST)
-  iwdfs.V_MBR_ACCT_HIST <- aliasDF(iwdfs.V_MBR_ACCT_HIST, "MBR_ACC")
+  iwdfs.V_MBR_ACCT_HIST <- aliasDF(iwdfs.V_MBR_ACCT_HIST, "MBR_ACC", TRUE)
   #add non null filter
   iwdfs.V_MBR_ACCT_HIST <- filter(iwdfs.V_MBR_ACCT_HIST, isNotNull(iwdfs.V_MBR_ACCT_HIST$MBR_ACC_EPH_LINK_ID))
 
   #load drug denorm
   iwdfs.V_DRUG_DENORM <- SelectDF(POC_MBR_OPPTY.V_DRUG_DENORM)
-  iwdfs.V_DRUG_DENORM <- aliasDF(iwdfs.V_DRUG_DENORM, "DRUG")
+  iwdfs.V_DRUG_DENORM <- aliasDF(iwdfs.V_DRUG_DENORM, "DRUG", TRUE)
 
 
   #join mbr acct and phmcy clm
@@ -60,43 +60,43 @@ prepare_p1_maxFillDtTable <- function(claimBaseTable){
   #select EPH_LINK_ID, GPI4_CD, MAX(FILL_DT) as max_fill_dt
   maxFillDtTable <- agg(maxFillDtTable, MAX_PHMCY_FILL_DT = max(claimBaseTable$PHMCY_FILL_DT))
 
-  maxFillDtTable <- aliasDF(maxFillDtTable, "MFDT")
+  maxFillDtTable <- aliasDF(maxFillDtTable, "MFDT", TRUE)
 
   return (maxFillDtTable)
 }
 
 prepare_p1_maxClmEventGid <- function(claimBaseTable) {
 
-#( select EPH_LINK_ID, GPI4_CD, T1.max_fill_dt, MAX(T0.CLM_EVNT_GID) as max_clm_evnt_gid
-#   FROM T0
-#   JOIN T1 ON T0.EPH_LINK_ID = T1.EPH_LINK_ID AND T0.GPI4_CD = T1.GPI4_CD AND T0.FILL_DT = T1.max_fill_dt
-#   GROUP BY EPH_LINK_ID, GPI4_CD , T1.max_fill_dt
-#)  T2
-# T1 is maxFillDtTable
+  #( select EPH_LINK_ID, GPI4_CD, T1.max_fill_dt, MAX(T0.CLM_EVNT_GID) as max_clm_evnt_gid
+  #   FROM T0
+  #   JOIN T1 ON T0.EPH_LINK_ID = T1.EPH_LINK_ID AND T0.GPI4_CD = T1.GPI4_CD AND T0.FILL_DT = T1.max_fill_dt
+  #   GROUP BY EPH_LINK_ID, GPI4_CD , T1.max_fill_dt
+  #)  T2
+  # T1 is maxFillDtTable
 
 
-maxFillDtTable <- prepare_p1_maxFillDtTable(claimBaseTable)
+  maxFillDtTable <- prepare_p1_maxFillDtTable(claimBaseTable)
 
 
-#join the tables
-# T0.EPH_LINK_ID = T1.EPH_LINK_ID AND T0.GPI4_CD = T1.GPI4_CD AND T0.FILL_DT = T1.max_fill_dt
-maxClmEventGidTable <- join(
-claimBaseTable, maxFillDtTable,
-claimBaseTable$MBR_ACC_EPH_LINK_ID == maxFillDtTable$MFDT_MBR_ACC_EPH_LINK_ID
-& claimBaseTable$DRUG_GPI4_CD == maxFillDtTable$MFDT_DRUG_GPI4_CD
-& claimBaseTable$PHMCY_FILL_DT == maxFillDtTable$MFDT_MAX_PHMCY_FILL_DT)
+  #join the tables
+  # T0.EPH_LINK_ID = T1.EPH_LINK_ID AND T0.GPI4_CD = T1.GPI4_CD AND T0.FILL_DT = T1.max_fill_dt
+  maxClmEventGidTable <- join(
+  claimBaseTable, maxFillDtTable,
+  claimBaseTable$MBR_ACC_EPH_LINK_ID == maxFillDtTable$MFDT_MBR_ACC_EPH_LINK_ID
+  & claimBaseTable$DRUG_GPI4_CD == maxFillDtTable$MFDT_DRUG_GPI4_CD
+  & claimBaseTable$PHMCY_FILL_DT == maxFillDtTable$MFDT_MAX_PHMCY_FILL_DT)
 
-#group by EPH_LINK_ID, GPI4_CD , T1.max_fill_dt
-maxClmEventGidTableGrp <- groupBy(maxClmEventGidTable,
-maxClmEventGidTable$MBR_ACC_EPH_LINK_ID, maxClmEventGidTable$DRUG_GPI4_CD, maxClmEventGidTable$PHMCY_FILL_DT)
+  #group by EPH_LINK_ID, GPI4_CD , T1.max_fill_dt
+  maxClmEventGidTableGrp <- groupBy(maxClmEventGidTable,
+  maxClmEventGidTable$MBR_ACC_EPH_LINK_ID, maxClmEventGidTable$DRUG_GPI4_CD, maxClmEventGidTable$PHMCY_FILL_DT)
 
-#select EPH_LINK_ID, GPI4_CD, T1.max_fill_dt, MAX(T0.CLM_EVNT_GID) as max_clm_evnt_gid
-maxClmEventGidTable <- agg(maxClmEventGidTableGrp,
-MAX_CLM_EVNT_GID = max(maxClmEventGidTable$PHMCY_CLM_EVNT_GID) )
+  #select EPH_LINK_ID, GPI4_CD, T1.max_fill_dt, MAX(T0.CLM_EVNT_GID) as max_clm_evnt_gid
+  maxClmEventGidTable <- agg(maxClmEventGidTableGrp,
+  MAX_CLM_EVNT_GID = max(maxClmEventGidTable$PHMCY_CLM_EVNT_GID) )
 
-maxClmEventGidTable <- aliasDF(maxClmEventGidTable, "MCEGT")
+  maxClmEventGidTable <- aliasDF(maxClmEventGidTable, "MCEGT", TRUE)
 
-return (maxClmEventGidTable)
+  return (maxClmEventGidTable)
 }
 
 
@@ -104,25 +104,27 @@ prepare_p1_LatestClaimTable <- function(iwdfs)  {
 
   claimBaseTable <- prepare_p1_ClaimBaseTableDF(iwdfs)
 
-maxClmEventGidTable <- prepare_p1_maxClmEventGid(claimBaseTable)
+  maxClmEventGidTable <- prepare_p1_maxClmEventGid(claimBaseTable)
 
 
-#(
-#select T0.*
-#  FROM T0
-#  JOIN T2 ON T0.MBR_ACC_EPH_LINK_ID = T2.MBR_ACC_EPH_LINK_ID AND T0.DRUG_GPI4_CD = T2.DRUG_GPI4_CD AND T0.PHMCY_FILL_DT = T2.PHMCY_FILL_DT AND T0.MAX_CLM_EVNT_GID = T2.MAX_CLM_EVNT_GID
-#) latestClaimTable
-# T0 = claimBaseTable  and T2 = maxClmEventGidTable
+  #(
+  #select T0.*
+  #  FROM T0
+  #  JOIN T2 ON T0.MBR_ACC_EPH_LINK_ID = T2.MBR_ACC_EPH_LINK_ID AND T0.DRUG_GPI4_CD = T2.DRUG_GPI4_CD AND T0.PHMCY_FILL_DT = T2.PHMCY_FILL_DT AND T0.MAX_CLM_EVNT_GID = T2.MAX_CLM_EVNT_GID
+  #) latestClaimTable
+  # T0 = claimBaseTable  and T2 = maxClmEventGidTable
 
-latestClaimBaseTable <- join(claimBaseTable, maxClmEventGidTable,
-claimBaseTable$MBR_ACC_EPH_LINK_ID == maxClmEventGidTable$MCEGT_MBR_ACC_EPH_LINK_ID
-& claimBaseTable$DRUG_GPI4_CD == maxClmEventGidTable$MCEGT_DRUG_GPI4_CD
-& claimBaseTable$PHMCY_FILL_DT == maxClmEventGidTable$MCEGT_PHMCY_FILL_DT
-& claimBaseTable$PHMCY_CLM_EVNT_GID == maxClmEventGidTable$MCEGT_MAX_CLM_EVNT_GID)
+  latestClaimBaseTable <- join(claimBaseTable, maxClmEventGidTable,
+  claimBaseTable$MBR_ACC_EPH_LINK_ID == maxClmEventGidTable$MCEGT_MBR_ACC_EPH_LINK_ID & claimBaseTable$DRUG_GPI4_CD == maxClmEventGidTable$MCEGT_DRUG_GPI4_CD & claimBaseTable$PHMCY_FILL_DT == maxClmEventGidTable$MCEGT_PHMCY_FILL_DT & claimBaseTable$PHMCY_CLM_EVNT_GID == maxClmEventGidTable$MCEGT_MAX_CLM_EVNT_GID)
 
-#Select T0.*
-col_names <- lapply(names(claimBaseTable), function(x) x)
-finalLatestClaimTable <- select(latestClaimBaseTable, col_names)
+  #Select T0.*
+  col_names <- lapply(names(claimBaseTable), function(x) x)
+  finalLatestClaimTable <- select(latestClaimBaseTable, col_names)
 
-return (finalLatestClaimTable)
+  print("VVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVV")
+  print(names(finalLatestClaimTable))
+  showDF(finalLatestClaimTable)
+  print("^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^")
+
+  return (finalLatestClaimTable)
 }
--- infoworks/iw_tools.R
+++ infoworks/iw_tools.R
@@ -1,8 +1,15 @@
 #Tools / Utils methods
 
-aliasDF <- function(df, alias_prefix) {
+aliasDF <- function(df, alias_prefix, uppercase = FALSE) {
   col_names <- names(df)
-  new_col_names <- lapply(col_names, function(x) paste(alias_prefix, "_", x, sep = ""))
+  new_col_names <- lapply(col_names,
+  function(x) {
+    c <- paste(alias_prefix, "_", x, sep = "")
+    if(uppercase) {
+      c <- toupper(c)
+    }
+    return (c)
+  })
   for(c in 1:length(col_names)){
     df <- withColumnRenamed(df, col_names[[c]], new_col_names[[c]])
   }
